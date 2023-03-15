6cb3e02f3635c4f8973e3c77bda63fd03f1c3b88
Mifrah Malik <mifrah.malik@gmail.com>
2023-03-15 13:50:25 -0400

    added a new lable for location into the addeventform

--- client/src/components/addeventform.js
+++ client/src/components/addeventform.js
@@ -1,8 +1,7 @@
 import { useState } from "react";
 
 const AddEventForm = () => {
-
-    //This is the state with the initial values empty"
+  //This is the state with the initial values empty"
   const [addevent, setAddEvent] = useState({
     title: "",
     location: "",
@@ -12,11 +11,11 @@ const AddEventForm = () => {
   //To reference event info above (note this comes from server.js and id is not handled in front-end)
   //{title: 'WIT Panel', location: 'Zoom', eventtime: "29/03/23"}
 
-const handleTitleChange = (e) => {
+  const handleTitleChange = (e) => {
     e.preventDefault();
     let titleUserGives = e.target.value;
     console.log(titleUserGives);
-}
+  };
 
   return (
     <form>
@@ -26,7 +25,17 @@ const handleTitleChange = (e) => {
         id="add-event-title"
         placeholder="The Title of your Event"
         required
-        value={event.title} //this indicates we would need state
+        value={addevent.title} //this indicates we would need state
+        onChange={handleTitleChange}
+      />
+
+      <label>Placeholder</label>
+      <input
+        type="text"
+        id="add-event-location"
+        placeholder="Location of your Event"
+        required
+        value={addevent.location} //this indicates we would need state
         onChange={handleTitleChange}
       />
     </form>
