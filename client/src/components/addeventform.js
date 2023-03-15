c4228c499264c87f15075719addb0fba7d46c8af
Mifrah Malik <mifrah.malik@gmail.com>
2023-03-15 10:48:41 -0400

    added in label and input information

--- client/src/components/addeventform.js
+++ client/src/components/addeventform.js
@@ -2,6 +2,19 @@ import { useState } from "react";
 
 const AddEventForm = () => {
 
+    //To reference event info (note this comes from server.js and id is not handled in front-end)
+    //{title: 'WIT Panel', location: 'Zoom', eventtime: "29/03/23"} 
+    <form>
+        <label>Event Title</label>
+            <input
+                type="text"
+                id="add-event-title"
+                placeholder="The Title of your Event"
+                required
+                value={event.title} //this indicates we would need state
+                onChange={handleTitleChange}
+    </>
+)
 }
 
 export default AddEventForm;
