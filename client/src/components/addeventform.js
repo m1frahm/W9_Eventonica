4765c78b3711956e6f74baa27bda42fa076c4faa
Mifrah Malik <mifrah.malik@gmail.com>
2023-03-15 11:00:26 -0400

    created handleTtitleChange function

--- client/src/components/addeventform.js
+++ client/src/components/addeventform.js
@@ -1,14 +1,23 @@
 import { useState } from "react";
 
 const AddEventForm = () => {
+
+    //This is the state with the initial values empty"
   const [addevent, setAddEvent] = useState({
     title: "",
     location: "",
     eventtime: "",
   });
 
-  //To reference event info (note this comes from server.js and id is not handled in front-end)
+  //To reference event info above (note this comes from server.js and id is not handled in front-end)
   //{title: 'WIT Panel', location: 'Zoom', eventtime: "29/03/23"}
+
+const handleTitleChange = (e) => {
+    e.preventDefault();
+    let titleUserGives = e.target.value;
+    console.log(titleUserGives);
+}
+
   return (
     <form>
       <label>Event Title</label>
