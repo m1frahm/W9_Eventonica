de8db323e0c341335a0e33f775d52fa284aa6746
Mifrah Malik <mifrah.malik@gmail.com>
2023-03-15 10:53:30 -0400

    fixed syntax errors and formatted

--- client/src/components/addeventform.js
+++ client/src/components/addeventform.js
@@ -1,22 +1,27 @@
 import { useState } from "react";
 
 const AddEventForm = () => {
+  const [addevent, setAddEvent] = useState({
+    title: "",
+    location: "",
+    eventtime: "",
+  });
 
-    const [addevent, setAddEvent] = useState({ title: "", location: "", eventtime: "" })
-
-    //To reference event info (note this comes from server.js and id is not handled in front-end)
-    //{title: 'WIT Panel', location: 'Zoom', eventtime: "29/03/23"} 
+  //To reference event info (note this comes from server.js and id is not handled in front-end)
+  //{title: 'WIT Panel', location: 'Zoom', eventtime: "29/03/23"}
+  return (
     <form>
-        <label>Event Title</label>
-            <input
-                type="text"
-                id="add-event-title"
-                placeholder="The Title of your Event"
-                required
-                value={event.title} //this indicates we would need state
-                onChange={handleTitleChange}
-    </>
-)
+      <label>Event Title</label>
+      <input
+        type="text"
+        id="add-event-title"
+        placeholder="The Title of your Event"
+        required
+        value={event.title} //this indicates we would need state
+        onChange={handleTitleChange}
+      />
+    </form>
+  );
 };
 
 export default AddEventForm;
