500167f0d389b0b51579bb22583d35188e62f63a
Mifrah Malik <mifrah.malik@gmail.com>
2023-03-16 17:05:28 -0400

    was doing some testing with using google font api

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,9 @@ function App() {
           />{" "}
         </div>
       </div>
+      <br></br>
+        <br></br>
+        <br></br>
       <Events />
     </div>
   );
--- client/src/components/events.js
+++ client/src/components/events.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import EventCard from "./event";
 import CardGroup from "react-bootstrap/CardGroup";
 import AddEventForm from "./addeventform";
+//import Index from './index.css';
 
 function Events() {
   const [events, setEvents] = useState([]); // using state to handle list of events
